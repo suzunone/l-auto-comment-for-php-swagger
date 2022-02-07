7ec690d81f128e394351e37768dec1bd8ea07405
suzunone <suzunone.eleven@gmail.com>
2022-02-07 14:10:28 +0900

    config install

--- src/AutoCommentForL5SwaggerServiceProvider.php
+++ src/AutoCommentForL5SwaggerServiceProvider.php
@@ -20,8 +20,14 @@ class AutoCommentForL5SwaggerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Register commands
-        $this->commands([L5SwaggerComment::class, ModelToOpenApiSchema::class]);
+        $configPath = __DIR__. '/config/auto-comment-for-l5-swagger.php';
+        if (function_exists('config_path')) {
+            $publishPath = config_path('auto-comment-for-l5-swagger.php');
+        } else {
+            $publishPath = base_path('config/auto-comment-for-l5-swagger.php');
+        }
+
+        $this->publishes([$configPath => $publishPath], 'config');
     }
 
     /**
@@ -31,5 +37,15 @@ class AutoCommentForL5SwaggerServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $configPath = __DIR__. '/config/auto-comment-for-l5-swagger.php';
+
+        // merge default config
+        $this->mergeConfigFrom(
+            $configPath,
+            'auto-comment-for-l5-swagger'
+        );
+
+        //Register commands
+        $this->commands([L5SwaggerComment::class, ModelToOpenApiSchema::class]);
     }
 }
